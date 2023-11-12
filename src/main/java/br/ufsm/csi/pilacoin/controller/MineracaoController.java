2c9042a7dad52a3c445616d7f209d5e338ca971b
Leonardo Henrique <leoilhaa@gmail.com>
2023-11-12 19:44:19 -0300

    MineracaoController

--- src/main/java/br/ufsm/csi/pilacoin/controller/MineracaoController.java
+++ src/main/java/br/ufsm/csi/pilacoin/controller/MineracaoController.java
@@ -1,5 +1,28 @@
 package br.ufsm.csi.pilacoin.controller;
 
+import br.ufsm.csi.pilacoin.service.DificuldadeService;
+import br.ufsm.csi.pilacoin.service.MineracaoPilaService;
+import br.ufsm.csi.pilacoin.service.RequisicoesService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/minerar")
 public class MineracaoController {
+
+
+    private final MineracaoPilaService mineracaoPilaService;
+
+    public MineracaoController( MineracaoPilaService mineracaoPilaService) {
+        this.mineracaoPilaService = mineracaoPilaService;
+    }
+
+
+    @PostMapping("/iniciar")
+    public void iniciarMineracao(@RequestParam int threads) {
+        mineracaoPilaService.iniciaMineracao();
+    }
 
 }
